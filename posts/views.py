9aab46f155ebebb56bfb05f564610b8479800b9e
Emrul <emrul.hoque@gmail.com>
2024-01-21 20:34:04 +0600

    Add initial files

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list' 
